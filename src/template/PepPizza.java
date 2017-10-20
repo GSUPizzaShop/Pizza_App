c298450efdcae44c460671936bffbae701b5af49
Bronson15 <bronsonlane@MacBook-2.local>
2017-10-20 16:07:17 -0400

    implementing decorator and template to work together

--- src/template/PepPizza.java
+++ src/template/PepPizza.java
@@ -1,36 +1,25 @@
 package template;
 
 public class PepPizza extends PizzaBuild{
-	double price;
 
 	void buildPizza() {
 		System.out.println("Building pep pizza");
 	}
 
-	void pizzaSize(String size) {
-		if(size.equalsIgnoreCase("small")) {
-			price = 4.29;
-			calPrice(price);
-		} else if(size.equalsIgnoreCase("medium")) {
-			price = 5.25;
-			calPrice(price);
-		} else if(size.equalsIgnoreCase("large")) {
-			price = 7.50;
-			calPrice(price);
-		}
+	@Override
+	public String getToppings() {
+		return "Crust, Cheese, Sauce, Pepperoni";
 	}
 
-	void addToppings() {
-		System.out.println("Adding marinara sauce");
-		System.out.println("Adding cheese");
-		System.out.println("Adding pep");
+	@Override
+	public double getCost() {
+		return 5.25;
 	}
 
-	double calPrice(double price) {
-		this.price = price;
-		double tax = .07;
-		price = price * tax;
-		return price;
+	@Override
+	public String getSize() {
+		return "medium";
 	}
+
 
 }
